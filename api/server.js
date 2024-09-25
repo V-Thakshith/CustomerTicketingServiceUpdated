9dff0723b1a6369fbe1ede27c05a311bd2d22748
VALMEEKI THAKSHITH <103265422+V-Thakshith@users.noreply.github.com>
2024-09-25 14:09:40 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Replace with the URL of your frontend application
+    origin: 'http://3.89.73.86:3000', // Replace with the URL of your frontend application
     credentials: true // Allow credentials (cookies, authorization headers, etc.)
 };
 
